099727bf3a2ac7397c29da628b69f8c1c5e3f426
Samuel Reams <34895348+spronkoid@users.noreply.github.com>
2018-12-13 15:09:58 -0600

    Made a smart decision
    omg mfkn yeet guys lets get this bread

---
+++ index.js
@@ -0,0 +1,7 @@
+var app = angular.module("myApp", []);
+app.controller('MainController', ['$scope', function($scope) {
+  
+  $scope.title = 'yeeeeeeeeeeeeeeeeet';
+  $scope.desco = 'mfkin yeet amirite';
+  $scope.omg = 'omg';
+}]);
